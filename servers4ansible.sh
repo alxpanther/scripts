38a85313f71fd52e246e2258d18c11f7e423075e
Alexander Fedorko <alx69@ukr.net>
2021-09-02 10:10:53 +0300

    many change

--- servers4ansible.sh
+++ servers4ansible.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 #
 # Created by Alexander Fedorko (02.09.2020)
@@ -6,6 +5,3 @@
 
 # for Ansible
 cat /var/named/*.local | egrep -va '^;|PTR' |grep -a " A "|egrep -va '^ns1|^ns2'|tr "." " " | awk '{print $1"."$2"."$3"\t\tansible_host="$6"."$7"."$8"."$9}'
-
-# for TASlife
-cat /var/named/tas.local.slave | egrep -va "^;|PTR"|grep -aP " A |\tA\t" |grep -vP "^\t"|egrep -va '^ns1|^ns2'|tr "." " " | awk '{print $1".taslife.com.ua\t\tansible_host="$3"."$4"."$5"."$6}'

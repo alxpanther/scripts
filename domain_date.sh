3deef4a0f3edfac36df4bb8ca5ab49eb5924a56f
Alexander Fedorko <alx69@ukr.net>
2022-01-10 15:18:46 +0200

    Search expire date for domains

---
+++ domain_date.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DOMAINS=( serveradmin.ru server.pro )
+
+THIS_DATE=`date +"%Y-%m-%d"`
+
+for domain in "${DOMAINS[@]}"
+  do
+    testing_domain=`whois $domain|egrep -i "expiry|expiries|paid-till" | grep -Eo '[0-9]{4}-[0-9]{2}-[0-9]{2}'`
+
+    if [ "$testing_domain" == "" ]
+      then
+        echo "Не удалось получить корректных данных по домену: $domain"
+
+    fi
+
+    exp_days=$(( ($(date -d $testing_domain  +%s) - $(date -d $THIS_DATE +%s)) / 86400 ))
+
+    if [ "$exp_days" -le 15 ]
+      then
+        echo "Домен $domain скоро проэкспайрится! Осталось $exp_days!"
+    fi
+
+    echo "Домену $domain еще $exp_days до отключения."
+
+done

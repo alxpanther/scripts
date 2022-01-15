2741a4834f353ce15afd26e8456bc1babd5c8495
Alexander Fedorko <alx69@ukr.net>
2022-01-15 13:39:40 +0200

    remove CHAT_ID and BOT_ID

--- mail_log2telegram/mail2telegram/search2telegram.sh
+++ mail_log2telegram/mail2telegram/search2telegram.sh
@@ -2,9 +2,6 @@
 
 export PATH=/root/.local/bin:/root/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/bin:/usr/local/sbin
 
-CHAT_ID="XXXX"        # Telegram Bot ID
-BOT_ID="botXXXXXXXX"  # Telegram Bot ID
-
 from_email=`echo $1 | awk '{print $8}' | xargs -d '\n' -IXXX  grep --line-buffered XXX logs/maillog | egrep --line-buffered 'esmtps' | awk '{print $10}'`
 
 /usr/bin/curl --connect-timeout 3 -s -X POST https://api.telegram.org/${BOT_ID}/sendMessage -d chat_id=${CHAT_ID} -d parse_mode="none" -d disable_web_page_preview="true" -d text="From E-Mail: $from_email%0A$1" > /dev/null

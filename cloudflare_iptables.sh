63515db1dc46d83e6f50cd08b44361e5017a5314
Alexander Fedorko <alx69@ukr.net>
2023-03-09 11:33:46 +0100

    fix ip6tables

--- cloudflare_iptables.sh
+++ cloudflare_iptables.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-#echo "iptables -N cloudflare"
+iptables -N cloudflare
+ip6tables -N cloudflare
 
 iptables -F cloudflare
+ip6tables -F cloudflare
 
 for IP in $(wget -q -O - https://www.cloudflare.com/ips-v4); do iptables -A cloudflare -s $IP -j ACCEPT; done
-for IP in $(wget -q -O - https://www.cloudflare.com/ips-v6); do iptables -A cloudflare -s $IP -j ACCEPT; done
+for IP in $(wget -q -O - https://www.cloudflare.com/ips-v6); do ip6tables -A cloudflare -s $IP -j ACCEPT; done
 
 iptables -A cloudflare -j DROP
+ip6tables -A cloudflare -j DROP
